9f5bd96e06055540a321d9def4835a3ac17406ec
grodier <grodier3@gmail.com>
2025-10-29 21:36:06 -0400

    Add static file server

--- internal/server/router.go
+++ internal/server/router.go
@@ -3,12 +3,14 @@ package server
 import (
 	"net/http"
 
+	"github.com/grodier/rss-go/internal/ui"
 	"github.com/julienschmidt/httprouter"
 )
 
 func (s *Server) router() http.Handler {
 	router := httprouter.New()
 
+	router.Handler(http.MethodGet, "/static/*file", http.FileServerFS(ui.Files))
 	router.HandlerFunc(http.MethodGet, "/", s.handleRootView)
 	router.HandlerFunc(http.MethodGet, "/feed/view/:id", s.handleFeedView)
 	router.HandlerFunc(http.MethodGet, "/feed/create", s.handleFeedCreate)
