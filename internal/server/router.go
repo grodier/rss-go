1d7fc5b2136801bc485310a55fc6d593b6dfb9e6
grodier <grodier3@gmail.com>
2025-10-29 22:04:01 -0400

    Prevent access static folder directory listings

--- internal/server/router.go
+++ internal/server/router.go
@@ -1,7 +1,10 @@
 package server
 
 import (
+	"embed"
+	"io/fs"
 	"net/http"
+	"path/filepath"
 
 	"github.com/grodier/rss-go/internal/ui"
 	"github.com/julienschmidt/httprouter"
@@ -10,7 +13,7 @@ import (
 func (s *Server) router() http.Handler {
 	router := httprouter.New()
 
-	router.Handler(http.MethodGet, "/static/*file", http.FileServerFS(ui.Files))
+	router.Handler(http.MethodGet, "/static/*file", http.FileServerFS(noDirListFS{ui.Files, s}))
 	router.HandlerFunc(http.MethodGet, "/", s.handleRootView)
 	router.HandlerFunc(http.MethodGet, "/feed/view/:id", s.handleFeedView)
 	router.HandlerFunc(http.MethodGet, "/feed/create", s.handleFeedCreate)
@@ -18,4 +21,33 @@ func (s *Server) router() http.Handler {
 	router.HandlerFunc(http.MethodGet, "/api/v1/healthcheck", s.handleHealthcheck)
 
 	return router
+}
+
+type noDirListFS struct {
+	fs embed.FS
+	s  *Server
+}
+
+func (n noDirListFS) Open(name string) (fs.File, error) {
+	f, err := n.fs.Open(name)
+	if err != nil {
+		return nil, err
+	}
+	stat, err := f.Stat()
+	if err != nil {
+		return nil, err
+	}
+	if stat.IsDir() {
+		n.s.logger.Info("attempt to list directory", "directory", name)
+		index := filepath.Join(name, "index.html")
+		if _, err := n.fs.Open(index); err != nil {
+			closeErr := f.Close()
+			if closeErr != nil {
+				return nil, closeErr
+			}
+
+			return nil, err
+		}
+	}
+	return f, nil
 }
