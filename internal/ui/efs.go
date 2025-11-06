46f60d0f79b50b3c5921a3d13bd2465b90dd846d
grodier <grodier3@gmail.com>
2025-11-05 19:58:32 -0500

    Refactor handling of file system with 404 for dir listings to ui package where files are embedded

--- internal/ui/efs.go
+++ internal/ui/efs.go
@@ -1,6 +1,39 @@
 package ui
 
-import "embed"
+import (
+	"embed"
+	"io/fs"
+	"path/filepath"
+)
 
 //go:embed "html" "static"
 var Files embed.FS
+
+type noDirEmbedFS struct {
+	fs embed.FS
+}
+
+var NoDirFiles = noDirEmbedFS{fs: Files}
+
+func (n noDirEmbedFS) Open(name string) (fs.File, error) {
+	f, err := n.fs.Open(name)
+	if err != nil {
+		return nil, err
+	}
+	stat, err := f.Stat()
+	if err != nil {
+		return nil, err
+	}
+	if stat.IsDir() {
+		index := filepath.Join(name, "index.html")
+		if _, err := n.fs.Open(index); err != nil {
+			closeErr := f.Close()
+			if closeErr != nil {
+				return nil, closeErr
+			}
+
+			return nil, err
+		}
+	}
+	return f, nil
+}
