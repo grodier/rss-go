aad3515d8f96de916b3aabcd6f689d4e046d0cea
grodier <grodier3@gmail.com>
2025-11-12 21:21:21 -0500

    Compose middleware with alice

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/grodier/rss-go
 go 1.25.2
 
 require github.com/julienschmidt/httprouter v1.3.0
+
+require github.com/justinas/alice v1.2.0 // indirect
--- internal/server/router.go
+++ internal/server/router.go
@@ -5,17 +5,21 @@ import (
 
 	"github.com/grodier/rss-go/internal/ui"
 	"github.com/julienschmidt/httprouter"
+	"github.com/justinas/alice"
 )
 
 func (s *Server) router() http.Handler {
 	router := httprouter.New()
 
 	router.Handler(http.MethodGet, "/static/*file", http.FileServerFS(ui.NoDirFiles))
+
 	router.HandlerFunc(http.MethodGet, "/", s.handleRootView)
 	router.HandlerFunc(http.MethodGet, "/feed/view/:id", s.handleFeedView)
 	router.HandlerFunc(http.MethodGet, "/feed/create", s.handleFeedCreate)
 	router.HandlerFunc(http.MethodPost, "/feed/create", s.handleFeedCreatePost)
 	router.HandlerFunc(http.MethodGet, "/api/v1/healthcheck", s.handleHealthcheck)
 
-	return s.recoverPanic(s.logRequest(commonHeaders(router)))
+	standardHeaders := alice.New(s.recoverPanic, s.logRequest, commonHeaders)
+
+	return standardHeaders.Then(router)
 }
